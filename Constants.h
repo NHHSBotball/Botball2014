60daa228dc803214c5e64b9471f11aa723286486
Henry Birge-Lee <henrybirgelee@gmail.com>
2014-06-20 11:41:40 -0700

    Adaptive test2.

---
+++ Constants.h
@@ -0,0 +1,31 @@
+/* 
+ * File:   Constants.h
+ * Author: Henry
+ *
+ * Created on June 20, 2014, 10:09 AM
+ */
+
+#ifndef CONSTANTS_H
+#define	CONSTANTS_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#define CLAW_UP_DOWN 0
+
+#define CLAW_FRONT_BACK 1
+
+#define CLAW_LEFT_RIGHT 2
+
+    
+#define CLAW_FRONT_THREASHOLD 891
+#define CLAW_LEFT_THREASHOLD 630
+    
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* CONSTANTS_H */
+
--- primary.c
+++ primary.c
@@ -5,10 +5,8 @@
 #include <stdbool.h>
 #include <string.h>
 #include <assert.h>
+#include "claw-utils.h"
 
-void move_claw_amount(int amount);
-void raw_raise_claw_to(int pos);
-void raise_claw_to(int pos);
 
 int main(int argc, char** argv) {
     
@@ -16,7 +14,8 @@ int main(int argc, char** argv) {
     //servos are at pickup 1200, ground is  240, dropoff is 600
     printf("starting v 5.\n");
     raise_claw_to(240);
-    enable_servos();
+    enable_servo(0);
+    enable_servo(2);
     raw_raise_claw_to(0);
     //set_servos_enabled(true);
     int* val = malloc(sizeof(int));
@@ -26,46 +25,10 @@ int main(int argc, char** argv) {
         scanf("%s %i", cmd, val);
         
         if (cmd[0] == 'c' || cmd[0] == 'C') {
-        move_claw_amount(*val);
+            move_claw_amount(*val);
         } else if (cmd[0] == 's' || cmd[0] == 'S') {
             raise_claw_to(*val);
         }
     }
     return (EXIT_SUCCESS);
-}
-
-void raise_claw_to(int pos) {
-    if (pos - get_servo_position(0) < -50) {
-        raw_raise_claw_to(get_servo_position(0) - 50);
-        msleep(100);
-        raise_claw_to(pos);
-    } else if (pos - get_servo_position(0) > 50) {
-        raw_raise_claw_to(pos);
-          
-    } else {
-        raw_raise_claw_to(pos);
-    }
-}
-
-void raw_raise_claw_to(int pos) {
-    
-    printf("raising claw to: %i\n", pos);
-        set_servo_position(0, pos);
-        set_servo_position(1, pos);
-        set_servo_position(2, 2047 - pos);
-        set_servo_position(3, 2047 - pos);
-}
-
-
-void move_claw_amount(int amount) {
-    printf("opening claw to: %i\n", amount);
-    if (amount < 0) {
-            motor(0, -100);
-            msleep(-amount);
-        } else {
-            motor(0, 100);
-            msleep(amount);
-        }
-    
-        motor(0, 0);
 }
